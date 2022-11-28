88ff9365679077895004a7731c511841ed9b9dd3
Sudip Poudel <107059003+sudipX@users.noreply.github.com>
2022-11-28 22:25:37 +0545

    Add files via upload

---
+++ Exercise_20_Reeborg_H1.py
@@ -0,0 +1,19 @@
+# Hurdle 1
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def jump():
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+    
+# for x in range(1,7):
+#     move()
+#     jump()
